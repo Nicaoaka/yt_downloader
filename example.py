729893e363f1c8d70bece3dd14ea385b3f81f0a0
Nicaoaka <obbstructed_sakura@gmail.com>
2026-08-29 15:10:27 -0700

    update the example

--- example.py
+++ example.py
@@ -15,41 +15,45 @@ config = PlaylistDL_Config(
     cookies_for_vids=False,
     empty_cookies=True,
     
-    write_flat=True,
+    write_raw_flat=True,
     write_raw_v_infos=True,
-    write_pl_info=True,
-    write_merge=True,
+
 
     refresh_after= 7 * 24 * 3600,
     force_flat_extract=False,
 
-    wrapper_match_filter=wrapper_match_filter_builder(
-        max_extracts = 25,
-        max_downloads = 5,
-        quit_when_maxed = True,
+    
+)
 
-        extract_match = lambda _: True, # extract whatever isn't downloaded
-        download_match = lambda v: (
-                 (v.get('view_count') or 0) < 250_000) # don't download popular videos
-            and ((v.get('duration') or 0)   < 5 * 60), # don't download long videos
+wrapper_match_filter=wrapper_match_filter_builder(
+    max_extracts = 25,
+    max_downloads = 5,
+    quit_when_maxed = True,
 
-        overrides = {
-            # DL_Action.USER:     [],
-            # DL_Action.QUIT:     [],
-            # DL_Action.SKIP:     [],
-            # DL_Action.EXTRACT:  [],
-            # DL_Action.DOWNLOAD: [],
-        },
+    extract_match = lambda _: True, # extract whatever isn't downloaded
+    download_match = lambda v: (
+                (v.get('view_count') or 0) < 250_000) # don't download popular videos
+        and ((v.get('duration') or 0)   < 5 * 60), # don't download long videos
 
-        fail_backoff_time = 7 * 24 * 3600,
-        ignore_ambiguous_dl_errors = True,
-        http_403_backoff_time = 3 * 24 * 3600,
+    overrides = {
+        # DL_Action.USER:     [],
+        # DL_Action.QUIT:     [],
+        # DL_Action.SKIP:     [],
+        # DL_Action.EXTRACT:  [],
+        # DL_Action.DOWNLOAD: [],
+    },
 
-        yt_unavailable_action = DL_Action.EXTRACT,
-    ),
+    fail_backoff_time = 7 * 24 * 3600,
+    ignore_ambiguous_dl_errors = True,
+    http_403_backoff_time = 3 * 24 * 3600,
+
+    yt_unavailable_action = DL_Action.EXTRACT,
+
+    debug=True,
 )
 
 with PlaylistDL(config) as pldl:
-    pldl.download(delete_prev_pl=True, delete_prev_merge=True)
-    pldl.display_pl_merge_timeline(pldl._infos.merge_info)
+    pldl.download_v_infos(wrapper_match_filter)
+    merge_info = pldl.make_merge_info(write=True)
+    pldl.display_pl_merge_timeline(merge_info)
     pldl.display_metadata_history()
